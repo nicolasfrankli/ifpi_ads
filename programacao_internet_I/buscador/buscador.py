3093845474de5506860c2a0085816f49a64d5100
Nicolas <nicolasgabrielolifranklin@gmail.com>
2023-03-02 16:30:41 -0300

    Adicionado exercício de Buscador

--- programacao_internet_I/buscador/buscador.py
+++ programacao_internet_I/buscador/buscador.py
@@ -17,17 +17,21 @@ def search(keyword, url, depth):
     if keyword in text:
         start_index = max(text.index(keyword) - 20, 0)
         end_index = min(text.index(keyword) + len(keyword) + 20, len(text))
-        result =  text[start_index:end_index]
+        result = text[start_index:end_index]
         results.append(result)
-
+      
+    count = 0 
     if depth > 0:
-        links = soup.find_all('a')
+        links = soup.find_all('a', href=True)
+        print(f'----------------Nível ${depth}----------------')
         for link in links:
-            href = link.get('href')
-            if href.startswith('http'):
-                results += search(keyword, href, depth - 1)
+            if link != '':
+                url = link.get('href')
+                if url.startswith('http'):
+                    print(f'Acessando ${url}')
+                    results += search(keyword, url, depth -1)
 
     return results
 
 
-print(search('Entenda','https://g4educacao.com/portal/o-que-e-blockchain', 0))
+print(search('Entenda','https://g4educacao.com/portal/o-que-e-blockchain', 2))
